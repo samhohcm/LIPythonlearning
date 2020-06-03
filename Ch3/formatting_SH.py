6c9fc7c0bbae24cd34a12831dc45b969b03e3d5e
samhohcm <samhohcm@gmail.com>
2020-06-04 00:31:39 +0100

    Finished Week 3

--- Ch3/formatting_SH.py
+++ Ch3/formatting_SH.py
@@ -8,21 +8,26 @@ def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
   now = datetime.now()
-  print(now.strftime())
-
+  
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+  
+  print(now.strftime("The current year is: %Y"))
+  print(now.strftime("%a, %d %B, %y"))
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
+  print(now.strftime("Locale date and time: %c"))
+  print(now.strftime("Locale date and time: %x"))
+  print(now.strftime("Locale date and time: %X"))  
+  #arrangement Based on locale set on computer
 
 
   #### Time Formatting ####
   
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
-
-
+  print(now.strftime("Current time: %I:%M:%S %p"))
+  print(now.strftime("24-H time: %H:%M:%S"))
 
 if __name__ == "__main__":
   main();
